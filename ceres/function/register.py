266da235746eb0e1d7c9784c955261cc67d57f0c
xuyongliang_01 <xuyongliang_yewu@cmss.chinamobile.com>
2023-10-18 05:56:32 +0000

    update ceres/function/register.py.
    Signed-off-by: xuyongliang_01 <xuyongliang_yewu@cmss.chinamobile.com>

--- ceres/function/register.py
+++ ceres/function/register.py
@@ -11,7 +11,6 @@
 # See the Mulan PSL v2 for more details.
 # ******************************************************************************/
 import json
-from typing import Dict
 
 import requests
 
