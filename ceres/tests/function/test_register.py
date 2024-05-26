a9a860942e65908279826b3ca0174cddb6be84b9
rabbitali <wenxin32@foxmail.com>
2024-05-26 21:55:40 +0800

    add testcase for register cmd

--- ceres/tests/function/test_register.py
+++ ceres/tests/function/test_register.py
@@ -12,7 +12,7 @@
 # ******************************************************************************/
 import unittest
 
-from ceres.manages.register_manage import register, register_info_to_dict
+from cli.register import RegisterCommand
 from ceres.function.status import PARAM_ERROR
 
 
@@ -26,7 +26,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_username_is_not_string(self):
@@ -39,7 +39,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_password_is_null(self):
@@ -51,7 +51,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_password_is_not_string(self):
@@ -64,7 +64,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_host_name_is_null(self):
@@ -76,7 +76,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_host_name_is_not_string(self):
@@ -89,7 +89,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_host_group_name_is_null(self):
@@ -101,7 +101,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_host_group_name_is_not_string(self):
@@ -114,7 +114,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_management_is_null(self):
@@ -126,7 +126,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_management_is_not_boolean(self):
@@ -139,7 +139,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_zeus_ip_is_null(self):
@@ -151,7 +151,7 @@ class TestRegister(unittest.TestCase):
             "management": False,
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_zeus_ip_is_not_string(self):
@@ -163,7 +163,7 @@ class TestRegister(unittest.TestCase):
             "management": False,
             "zeus_port": "11111",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_zeus_port_is_null(self):
@@ -175,7 +175,7 @@ class TestRegister(unittest.TestCase):
             "management": False,
             "zeus_ip": "127.0.0.1",
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_zeus_port_is_not_string(self):
@@ -188,7 +188,7 @@ class TestRegister(unittest.TestCase):
             "zeus_ip": "127.0.0.1",
             "zeus_port": 80,
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(PARAM_ERROR, data)
 
     def test_register_should_return_param_error_when_input_ceres_port_is_not_string(self):
@@ -202,20 +202,20 @@ class TestRegister(unittest.TestCase):
             "zeus_port": "11111",
             "ceres_port": 11000,
         }
-        data = register(input_data)
+        data = RegisterCommand._register_handle(input_data)
         self.assertEqual(data, PARAM_ERROR)
 
     def test_register_info_to_dict_should_return_dict_info_when_input_is_correct(self):
         mock_string = '{"mock": "mock"}'
-        res = register_info_to_dict(mock_string)
+        res = RegisterCommand._register_info_to_dict(mock_string)
         self.assertEqual({'mock': 'mock'}, res)
 
     def test_register_info_to_dict_should_return_empty_dict_when_input_is_incorrect(self):
         mock_string = '["mock"]'
-        res = register_info_to_dict(mock_string)
+        res = RegisterCommand._register_info_to_dict(mock_string)
         self.assertEqual({}, res)
 
     def test_register_info_to_dict_should_return_empty_dict_when_input_is_not_json_string(self):
         mock_string = '{mock'
-        res = register_info_to_dict(mock_string)
+        res = RegisterCommand._register_info_to_dict(mock_string)
         self.assertEqual({}, res)
